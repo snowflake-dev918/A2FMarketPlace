a3cfc5ae33f34e8b3be3d0360a886eee6a688ad5
gentle-smile918 <gentlesmile918@gmail.com>
2022-03-30 01:12:32 -0700

    errors fixed

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import * as ReactDOMClient from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Web3ReactProvider } from "@web3-react/core";
+import Web3 from "web3";
 
-ReactDOM.render(
+function getLibrary(provider) {
+  return new Web3(provider);
+}
+
+const container = document.getElementById("root");
+
+const root = ReactDOMClient.createRoot(container);
+
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <App />
+    </Web3ReactProvider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
